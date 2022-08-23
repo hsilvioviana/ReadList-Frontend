a7eb13737dfc6bd3896effcfa07307730b5a4a71
Silvio Viana <hsilvioviana@hotmail.com>
2022-08-23 07:32:11 -0300

    feat: more pages files added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,6 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', 
-    redirectTo: '/login', 
-    pathMatch: 'full' 
-  },
   {
     path: 'login',
     loadChildren: () => import('./pages/login/login.module').then(m => m.LoginModule)
@@ -13,6 +9,22 @@ const routes: Routes = [
   {
     path: 'signup',
     loadChildren: () => import('./pages/signup/signup.module').then(m => m.SignupModule)
+  },
+  {
+    path: '',
+    loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule)
+  },
+  {
+    path: 'statistics',
+    loadChildren: () => import('./pages/statistics/statistics.module').then(m => m.StatisticsModule)
+  },
+  {
+    path: 'configs',
+    loadChildren: () => import('./pages/configs/configs.module').then(m => m.ConfigsModule)
+  },
+  {
+    path: '**',
+    loadChildren: () => import('./pages/error/error.module').then(m => m.ErrorModule)
   }
 ];
 
